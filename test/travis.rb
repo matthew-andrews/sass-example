d3619325fc7443bb893607b3a4390ace47a8d2de
Matt Andrews <matt@mattandre.ws>
2014-01-18 22:56:19 +0000

    Add the build directory

--- test/travis.rb
+++ test/travis.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 # Compile o-colors normally
+`mkdir build`
 `sass main.scss build/main.css`
 raise "When compiled the module should output some CSS" unless File.exists?('build/main.css');
 puts "Regular compile worked successfully"
