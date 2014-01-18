fd90a17b88e9018f8e5cefc96671bf7f19f27404
Matt Andrews <matt@mattandre.ws>
2014-01-18 23:07:28 +0000

    Remove unrelated comment

--- test/travis.rb
+++ test/travis.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-
-# Compile o-colors normally
 `sass main.scss built.css`
 raise "When compiled the module should output some CSS" unless File.exists?('built.css');
 puts "Regular compile worked successfully"
