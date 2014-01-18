85279fa5f640cedcdcb9c648c14fdf61b16848da
Matt Andrews <matt@mattandre.ws>
2014-01-18 23:48:27 +0000

    Use  not back ticks

--- test/travis.rb
+++ test/travis.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-`sass main.scss built.css`
+exec('sass main.scss built.css');
 raise "When compiled the module should output some CSS" unless File.exists?('built.css');
 puts "Regular compile worked successfully"
